54e8efb13d22a1c26653a1ba3811d6dc974e8077
spr92 <bpaydar7@gmail.com>
2018-06-19 15:54:29 +0430

    skills and general update

--- files/scripts.js
+++ files/scripts.js
@@ -18,3 +18,22 @@ function styleLink() {
         }
     });
 }
+
+function hideSkills(e) {
+    $(e.target).parent().removeClass("show");
+}
+
+function showSkills(e) {
+    var skills=$(e.target).next();
+    skills.addClass("show");
+    skills.children("button").click(hideSkills);
+}
+
+function init(){
+    layoutPlanner();
+    styleLink();
+    if(window.location.href.indexOf("skill.html") !== -1){
+        var openers=$(".skillGroup > h3");
+        openers.click(showSkills);
+    }
+}
